c1f4d43d11e6a5dbe6f6762f0e1d628e1f603805
Neonebu <oguzcansirolu1@gmail.com>
2021-04-20 00:08:36 +0300

    Updated Home Screen

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(function () {
+
+    var img = document.getElementById('leftPanel').firstChild;
+    img.onload = function () {
+        if (img.height > img.width) {
+            img.height = '100%';
+            img.width = 'auto';
+        }
+    };
+
+}());
